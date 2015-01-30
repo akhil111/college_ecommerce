5c5447938b602a705add141f9a3f8c4ac61294b1
Your Name <you@example.com>
2015-01-30 18:53:48 +0530

    this is default code

---
+++ akhil.php
@@ -0,0 +1,110 @@
+CTYPE html>
+<head>
+    
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    
+    <style>
+        
+        body{
+        
+            
+            font-family: 'Lobster', cursive;
+            text-align: center;
+            background: black;
+            color:white;
+    /*         background: url("http://dash.ga.co/assets/anna-bg.png");*/
+            
+           /* background:url('http://www.hdwallpapersinn.com/wp-content/uploads/2014/09/Golden-Gate-Bridge-HD-Photo-Wallpaper.jpg');*/
+      
+            background:url('http://www.hdwallpapersinn.com/wp-content/uploads/2014/09/Golden-Gate-Bridge-HD-Photo-Wallpaper.jpg');
+
+       /* background:url('girl.png');*/
+      background-repeat:no-repeat;
+
+      background-size:cover;
+      background-position:center;
+        }
+
+
+@media screen and (max-width: 300px) {
+    body {
+        background-color: lightblue;
+        background:url('newyork.png');
+    }
+
+     input[type="submit"]
+     {
+
+   color:tomato;
+
+     }
+}
+       
+
+        
+        #my-pic{
+        
+            width: 200px;
+            height: 200px;
+            border-radius: 75px;
+             border:4px solid white;
+            margin-top: 59px;
+        }
+        
+        input[type="email"]
+        
+        {
+            border:0;
+        text-align: center;
+            padding:10px;
+            margin-right: 5px;
+            font-family: 'Lobster', cursive;
+            font-weight: 400;
+            color:hotpink;
+            font-size: 22px;
+            border-radius: 5px;
+            
+        }
+        
+        input[type="submit"]
+        
+        {
+        color: white;
+            background: #5fcf80;
+            padding: 10px;
+            font-family: 'Lobster', cursive;
+            font-size: 22px;
+            width: 180px;
+            border-radius: 10px;
+        border:2px solid white;
+            
+        
+        }
+        
+         input[type="submit"]:hover{
+         color:  #5fcf80;
+             background: white;
+         
+         
+         }
+        
+        
+        </style>
+    
+    
+    </head>
+
+<body>
+ 
+    <img src="http://cdn.koimoi.com/wp-content/new-galleries/2012/10/Huma-Qureshi-At-Chicken-Khurana-Recipe-Hunt-Launch-560x747.jpg" id="my-pic">
+    <h1>my name is akhil , india based rails developer</h1>
+    <input type="email" placeholder="type your email">
+    
+    <input type="submit">
+    
+</body>
+
+
+
+</html>
+
