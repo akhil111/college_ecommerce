852027334c44eba43f180f7c2ec0e9520bb34fa2
akhil111 <iamgooglefan111>
2016-03-22 19:50:50 +0530

    form data gets stored mysql tables

--- book_upload.php
+++ book_upload.php
@@ -1,28 +1,41 @@
-
 <?php
 
 
 include_once 'putin.php';
-
+include_once 'functions.php';
+if($conn)
+{
+	echo 'connection is successful';
+}
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 
-<link href='https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
 <style>
 
-/*body{
-	background-image: linear-gradient(45deg,#0a1855 0%,#da0024 100%);
-}*/
+.alert
+{
+	width:340px;
+	height: 60px;
+	position: absolute;
+	top: 10px;
+	left: 950px;
+}
 
 div
 {
 	border:2px solid black;
-width: 550px;
-height: 400px;
+width: 560px;
+height: 450px;
 background-color: #DFE2DB
 ;
 border-color: white;
@@ -51,7 +64,7 @@ form input[type=text], form input[type=email], form input[type=password]
     font-family: 'Libre Baskerville', serif;
 }
 
-button
+input[type="submit"]
 {
 	background-color: #4db538;
 	width:52%;
@@ -64,7 +77,7 @@ button
     border-radius: 5px;
 }
 
-button:hover
+input[type="submit"]:hover
 {
 	 color: #4db538;
 	 background-color: #FFFFFF;
@@ -75,29 +88,73 @@ button:hover
 
 
 </style>
-
-<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
-<script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script> -->
-
 </head>
-<body style="background-color: #7E8F7C;">
 
+
+<body style="background-color:white;">
 <center>
 <div>
-
-
-<form id="book_upload_form">
+<form action="book_upload.php" method="post" enctype="multipart/form-data">
 
 <input type="text" name="book_name" placeholder="enter book name"><br>
 <input type="text" name="book_price" placeholder="enter book price"><br>
 <input type="text" name="book_branch" placeholder="book branch"><br>
-<input type="file" style="padding: 15px; margin-top: 30px;"><br>
-<button type="button">submit</button>
+<input type="file" name="book_image" style="padding: 15px; margin-top: 30px;"><br>
+<!-- <button type="button">submit</button> -->
 
+<input type="submit" name="submit"/>
 </form>
 </div>
 
 </center>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
+
+
+
+<?php
+
+if(isset($_POST['submit']))
+
+{
+	
+	$book_name=$_POST['book_name'];
+		$book_pr=$_POST['book_price'];
+			$book_branch=$_POST['book_branch'];
+			
+			$akhil=$_FILES['book_image']['name'];
+
+
+$query="INSERT INTO book_details VALUES('$book_name','$book_pr','$book_branch','$akhil')";
+
+if($conn->query($query)==TRUE)
+
+{
+
+
+echo '
+ 
+<div class="alert alert-success">upload super successful.</div>
+
+
+';
+
+}
+
+else
+{
+	/*echo "<h1>book upload not  successful</h1>";*/
+	  echo '<div class="alert alert-danger">upload not successful.</div>';
+}
+
+}
+
+echo $_SERVER['REMOTE_ADDR'];
+
+?>
+
+
+
